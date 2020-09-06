f8acb300a496a7bab0f1ddaa05bfb9e34d4e62df
yrinaldi <yesica_rinaldi@hotmail.com>
2020-09-06 19:51:30 -0300

    subo clase 2 - ejercicio 17

---
+++ Clase 2/Ejercicio17.java
@@ -0,0 +1,16 @@
+package Clase2;
+//17) Imprimir los números del 1 al 10.
+
+import java.util.Scanner;
+
+public class Ejercicio17 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int i = 0;
+        for (i = 1; i<=10; i++){
+        System.out.println(i);
+
+        }
+   }
+}
